3276059f4c15f443df2478aef15efd03fd1f11b6
LeHaine <colt.daily@gmail.com>
2021-04-21 10:55:21 -0400

    update state machine to use transitions

--- src/commonMain/kotlin/com/lehaine/kiwi/StateMachine.kt
+++ src/commonMain/kotlin/com/lehaine/kiwi/StateMachine.kt
@@ -1,6 +1,5 @@
 package com.lehaine.kiwi
 
-import com.soywiz.kds.iterators.fastForEach
 import com.soywiz.klock.TimeSpan
 
 @DslMarker
@@ -8,39 +7,36 @@ annotation class StateMachineDsl
 
 data class State<STATE : Any>(
     val type: STATE,
-    val reason: () -> Boolean,
+    val transition: () -> STATE,
     val update: (dt: TimeSpan) -> Unit,
     val begin: () -> Unit,
     val end: () -> Unit
 )
 
-class StateMachine<STATE : Any>(private val states: ArrayList<State<out STATE>>) {
+class StateMachine<STATE : Any>(private val states: HashMap<STATE, State<out STATE>>, initialState: STATE) {
 
     var onStateChanged: ((STATE) -> Unit)? = null
-    private var _currentState: State<out STATE>? = null
+    private var _currentState: State<out STATE> =
+        states[initialState] ?: error("Unable to set initial state of: $initialState")
     val currentState get() = _currentState
 
     fun update(dt: TimeSpan) {
-        states.fastForEach { state ->
-            if (state.reason()) {
-                if (_currentState != state) {
-                    _currentState?.end?.invoke()
-                    _currentState = state
-                    _currentState?.begin?.invoke()
-                    _currentState?.also { onStateChanged?.invoke(it.type) }
-                }
-                _currentState?.update?.invoke(dt)
-                return
-            }
+        val state = _currentState.transition()
+        if (_currentState.type != state) {
+            _currentState.end.invoke()
+            _currentState = states[state] ?: error("Unable to set state of: $state")
+            _currentState.begin.invoke()
+            _currentState.also { onStateChanged?.invoke(it.type) }
         }
+        _currentState.update.invoke(dt)
     }
 
     @StateMachineDsl
-    class StateMachineBuilder<STATE : Any> {
-        private val states = arrayListOf<State<out STATE>>()
+    class StateMachineBuilder<STATE : Any>(private val initialState: STATE) {
+        private val states = hashMapOf<STATE, State<out STATE>>()
         private var onStateChanged: (STATE) -> Unit = {}
-        fun <S : STATE> state(type: S, action: StateBuilder<S>.() -> Unit) {
-            states.add(StateBuilder<S>().apply(action).build(type))
+        fun state(type: STATE, action: StateBuilder<STATE>.() -> Unit) {
+            states[type] = StateBuilder(type).apply(action).build(type)
         }
 
         fun stateChanged(onStateChanged: (STATE) -> Unit) {
@@ -48,20 +44,23 @@ class StateMachine<STATE : Any>(private val states: ArrayList<State<out STATE>>)
         }
 
         fun build(): StateMachine<STATE> {
-            return StateMachine(states).apply { this.onStateChanged = this@StateMachineBuilder.onStateChanged }
+            return StateMachine(states, initialState).apply {
+                this.onStateChanged = this@StateMachineBuilder.onStateChanged
+            }
         }
     }
 }
 
 @StateMachineDsl
-class StateBuilder<STATE : Any> {
+class StateBuilder<STATE : Any>(type: STATE) {
 
-    private var reason: () -> Boolean = { false }
+    private var transition: () -> STATE = { type }
     private var update: (dt: TimeSpan) -> Unit = {}
     private var begin: () -> Unit = {}
     private var end: () -> Unit = {}
-    fun reason(reason: () -> Boolean) {
-        this.reason = reason
+
+    fun <S : STATE> transition(transition: () -> S) {
+        this.transition = transition
     }
 
     fun update(update: (dt: TimeSpan) -> Unit) {
@@ -77,10 +76,13 @@ class StateBuilder<STATE : Any> {
     }
 
     fun build(type: STATE): State<STATE> {
-        return State(type, reason, update, begin, end)
+        return State(type, transition, update, begin, end)
     }
 }
 
-fun <STATE : Any> stateMachine(action: StateMachine.StateMachineBuilder<STATE>.() -> Unit): StateMachine<STATE> {
-    return StateMachine.StateMachineBuilder<STATE>().apply(action).build()
+fun <STATE : Any> stateMachine(
+    initialState: STATE,
+    action: StateMachine.StateMachineBuilder<STATE>.() -> Unit
+): StateMachine<STATE> {
+    return StateMachine.StateMachineBuilder(initialState).apply(action).build()
 }
